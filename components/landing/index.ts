f8be16dfa71235286e881a08dadd13456a0eb427
williamsburgsh-commits <williamsburg.sh@gmail.com>
2025-11-17 17:49:06 +0200

    Configure env and landing exports

--- components/landing/index.ts
+++ components/landing/index.ts
@@ -3,4 +3,3 @@ export { HowItWorksStep } from './HowItWorksStep';
 export { ComparisonTable } from './ComparisonTable';
 export { TechStackGrid } from './TechStackGrid';
 export { CTABlock } from './CTABlock';
-export { default as Hero3D } from './Hero3D';
